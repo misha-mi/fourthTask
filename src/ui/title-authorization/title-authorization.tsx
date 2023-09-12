4388dffc461985b75da993bada1f46a2ce3075bf
Misha <misha.saighanov@mail.ru>
2023-09-12 18:08:09 +0600

    create title-authorization component

---
+++ src/ui/title-authorization/title-authorization.tsx
@@ -0,0 +1,40 @@
+import { useTheme } from '@react-navigation/native';
+import { View, StyleSheet, Text } from 'react-native';
+
+interface ITitleAuthorization {
+  titleText: string;
+  subtitleText: string;
+}
+
+const TitleAuthorization = ({
+  titleText,
+  subtitleText,
+}: ITitleAuthorization) => {
+  const { titleColor, subtitleColor } =
+    useTheme().colors.titleAuthorizationColors;
+
+  return (
+    <View>
+      <Text style={[styles.text, styles.fs32, { color: titleColor }]}>
+        {titleText}
+      </Text>
+      <Text style={[styles.text, styles.fs16, { color: subtitleColor }]}>
+        {subtitleText}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Outfit-Regular',
+  },
+  fs32: {
+    fontSize: 32,
+  },
+  fs16: {
+    fontSize: 16,
+  },
+});
+
+export default TitleAuthorization;
